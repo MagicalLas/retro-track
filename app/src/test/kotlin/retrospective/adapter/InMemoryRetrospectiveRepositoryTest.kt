b0abd90d90de8f7bcb36927e6aebdfc9dda4f831
Las <haho013039@gmail.com>
2021-02-25 23:06:42 +0900

    repository에서 유저 이름으로 회고를 가져오는 테스트 작성

--- app/src/test/kotlin/retrospective/adapter/InMemoryRetrospectiveRepositoryTest.kt
+++ app/src/test/kotlin/retrospective/adapter/InMemoryRetrospectiveRepositoryTest.kt
@@ -1,8 +1,12 @@
 package retrospective.adapter
 
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 import retrospective.domain.Retrospective
+import strikt.api.expectThat
+import strikt.assertions.contains
+import strikt.assertions.hasSize
 
 object InMemoryRetrospectiveRepositoryTest: Spek({
     Feature("save retrospective") {
@@ -18,6 +22,23 @@ object InMemoryRetrospectiveRepositoryTest: Spek({
             }
             Then("repository 에서 저장한 회고를 찾을 수 있다") {
                 repository.findById(retro.id)
+            }
+        }
+
+        Scenario("회고 가져오기") {
+            lateinit var retro: Retrospective
+            lateinit var foundRetroList: List<Retrospective>
+            val repository = InMemoryRetrospectiveRepository()
+
+            Given("회고가 이미 저장되었을 때") {
+                retro = Retrospective(0L, "Las", "우유를 먹으면 배가 아파")
+                repository.save(retro)
+            }
+            When("특정 유저의 회고들을 가져오는 경우") {
+                foundRetroList = repository.findByUserName(retro.userName)
+            }
+            Then("repository 에서 특정 유저가 저장한 회고를 모두 찾을 수 있다") {
+                expectThat(foundRetroList).hasSize(1).contains(retro)
             }
         }
     }
